f018ea5dc406b9dc2ea5bd8a7311634096750746
tamberg <tamberg@yaler.net>
2026-03-18 23:32:17 +0100

    Update.

--- binary.c
+++ binary.c
@@ -6,16 +6,26 @@ char *svg_doc_start =
 
 char *svg_pattern =
     "  <g transform=\"translate(%dmm %dmm)\">\n"
-    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"10.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
-    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"12.5mm\" fill=\"none\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
-    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"15.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
-    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"10.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
-    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"12.5mm\" fill=\"none\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
-    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"15.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"10.0mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"12.5mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"15.0mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"10.0mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"12.5mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"15.0mm\" fill=\"%s\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
     "  </g>\n";
 
 char *svg_doc_end = 
     "</svg>\n";
+
+void print_pattern(unsigned char value, int x, int y) {
+   char *dot1 = (value >> 5) & 1 ? "black" : "none";
+   char *dot2 = (value >> 4) & 1 ? "black" : "none";
+   char *dot3 = (value >> 3) & 1 ? "black" : "none";
+   char *dot4 = (value >> 2) & 1 ? "black" : "none";
+   char *dot5 = (value >> 1) & 1 ? "black" : "none";
+   char *dot6 = (value >> 0) & 1 ? "black" : "none";
+   printf(svg_pattern, x, y, dot1, dot2, dot3, dot4, dot5, dot6);
+}
 
 void print_bin(unsigned char value) {
     printf("0b%d%d%d%d%d%d%d%d\n",
@@ -46,9 +56,9 @@ int main(void) {
    print_bin("⠓"[2]);
 
    printf("%s", svg_doc_start);
-   printf(svg_pattern, 6, 6);
-   printf(svg_pattern, 16, 6);
-   printf(svg_pattern, 22, 6);
+   print_pattern(0b00000000, 6, 6);
+   print_pattern(0b00101010, 16, 6);
+   print_pattern(0b00111111, 22, 6);
    printf("%s", svg_doc_end);
    return 0;
 }
