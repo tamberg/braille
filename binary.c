e8528e01664767d78fa68f5e9b77644c9eaffd52
tamberg <tamberg@yaler.net>
2026-03-18 23:18:27 +0100

    Update.

--- binary.c
+++ binary.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+char *svg_doc_start = 
+    "<svg height=\"100%\" width=\"100%\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+
+char *svg_pattern =
+    "  <g transform=\"translate(%dmm %dmm)\">\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"10.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"12.5mm\" fill=\"none\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"10.0mm\" cy=\"15.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"10.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"12.5mm\" fill=\"none\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "    <circle r=\"0.8mm\" cx=\"12.5mm\" cy=\"15.0mm\" fill=\"black\" stroke=\"black\" stroke-width=\"0.01mm\" />\n"
+    "  </g>\n";
+
+char *svg_doc_end = 
+    "</svg>\n";
 
 void print_bin(unsigned char value) {
     printf("0b%d%d%d%d%d%d%d%d\n",
@@ -22,5 +39,16 @@ int main(void) {
    print_bin(0b00100000);
    print_bin(0b01000000);
    print_bin(0b10000000);
+   printf("\n");
+   printf("%lu\n", strlen("⠓"));
+   print_bin("⠓"[0]);
+   print_bin("⠓"[1]);
+   print_bin("⠓"[2]);
+
+   printf("%s", svg_doc_start);
+   printf(svg_pattern, 6, 6);
+   printf(svg_pattern, 16, 6);
+   printf(svg_pattern, 22, 6);
+   printf("%s", svg_doc_end);
    return 0;
 }
